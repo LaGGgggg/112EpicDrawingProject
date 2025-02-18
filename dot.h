e88f03a4727e9ea3e21191e345d700295cc651c6
77maxikov <77maxikov@mail.ru>
2025-02-18 10:47:31 +0300

    Update dot.h

--- dot.h
+++ dot.h
@@ -1,8 +1,15 @@
 #ifndef DOT_H
 #define DOT_H
 
-struct dot {
-  double x, y;
+class  dot {
+public:
+	dot(double x = 0.0, double y = 0.0):m_x(x),m_y(y) {
+		m_id = generateID();
+	}
+	double getx()const { return m_x; }
+	double gety()const { return m_y; }
+private:
+	ID m_id;
+	double m_x, m_y;
 };
-
 #endif // DOT_H
