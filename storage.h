1b7570e4fdd0a0bffd9086a012976d56db86a6c6
77maxikov <77maxikov@mail.ru>
2025-02-18 10:50:24 +0300

    Update storage.h

--- storage.h
+++ storage.h
@@ -5,40 +5,47 @@
 #include "segment.h"
 #include <cassert>
 #include <cstddef>
+#include <cassert>
 
-struct dotStorage {
-  dot* storage;
-  size_t size;
+template<typename El> class Storage {
+public:
+	Storage() {
+
+	}
+	void add(const El& d) {
+		El* newStorage = new El[m_size + 1];
+		// Копируем имеющееся содержимое 
+		for (size_t k = 0; k < m_size; ++k)
+			newStorage[k] = m_storage[k];
+		newStorage[m_size] = d;
+		delete[] m_storage;
+		m_storage = newStorage;
+		++m_size;
+	}
+private:
+	El* m_storage;
+	size_t m_size;
 };
-
-struct segmentStorage {
-  segment* storage;
-  size_t size;
-};
-
-template <typename Storage, typename El>
-void add(Storage& s, const El& d) {
-  El* newStorage = new El[s.size + 1];
-  for (size_t k = 0; k < s.size; ++k)
-    newStorage[k] = s.storage[k];
-  newStorage[s.size] = d;
-  delete[] s.storage;
-  s.storage = newStorage;
-  ++s.size;
-}
 
 template <typename Storage, typename El>
 void remove(Storage& s, size_t num) {
-  assert(num < s.size);
-  El* newStorage = new El[s.size - 1];
-  for (size_t k = 0; k < num; ++k)
-    newStorage[k] = s.storage[k];
-  for (size_t k = num + 1; k < s.size; ++k)
-    newStorage[k - 1] = s.storage[k];
-  delete[] s.storage;
-  s.storage = newStorage;
-  --s.size;
+	assert(num < s.size);
+	/*if (num > s.size + 1) 
+		throw std::out_of_range("Incorrect index");	
+		*/
+
+	El* newStorage = new El[s.size - 1];
+	// Копируем имеющееся содержимое 
+	for (size_t k = 0; k < num; ++k)
+		newStorage[k] = s.storage[k];
+	for (size_t k = num+1; k < s.size; ++k)
+		newStorage[k-1] = s.storage[k];
+		
+	delete[] s.storage;
+	s.storage = newStorage;
+	--s.size;
 }
+
 
 dotStorage createTestDotStorage();
 
