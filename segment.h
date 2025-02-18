ba3171033794ab5e8932bdccb1d797760a2b4672
77maxikov <77maxikov@mail.ru>
2025-02-18 10:48:15 +0300

    Update segment.h

--- segment.h
+++ segment.h
@@ -2,10 +2,17 @@
 #define SEGMENT_H
 
 #include "dot.h"
+#include "idgenerator.h"
 
-struct segment {
-  dot start;
-  dot end;
+class segment {
+public:
+	segment(const dot&s = dot(), const dot& e = dot()):m_start(s),m_end(e) {
+		m_id = generateID();
+	}
+private:
+	ID m_id;
+	dot m_start;
+	dot m_end;
 };
 
 #endif // SEGMENT_H
