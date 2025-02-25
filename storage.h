3f71aa40bcb8604686c720350c2742ee48ea1bd2
Arseny-P <catdog09073@gmail.com>
2025-02-25 11:03:52 +0300

    fix: fix problems from last commit

--- storage.h
+++ storage.h
@@ -9,68 +9,51 @@
 template<typename El>
 class Storage {
 public:
-<<<<<<< HEAD
-  Storage() : m_storage(nullptr), m_size(0) {}
+    Storage() : m_storage(nullptr), m_size(0) {}
 
-  void add(const El& d) {
-    El* newStorage = new El[m_size + 1];
-    for (size_t k = 0; k < m_size; ++k)
-      newStorage[k] = m_storage[k];
-    newStorage[m_size] = d;
-    delete[] m_storage;
-    m_storage = newStorage;
-    ++m_size;
-  }
+    void add(const El& d) {
+        El* newStorage = new El[m_size + 1];
+        // Копируем имеющееся содержимое 
+        for (size_t k = 0; k < m_size; ++k)
+            newStorage[k] = m_storage[k];
+        newStorage[m_size] = d;
+        delete[] m_storage;
+        m_storage = newStorage;
+        ++m_size;
+    }
+    void remove(size_t num) {
+        assert(num < m_size);
 
-  void remove(size_t num) {
-    assert(num < m_size);
-  
-    El* newStorage = new El[m_size - 1];
-    // Копируем имеющееся содержимое 
-    for (size_t k = 0; k < num; ++k)
-      newStorage[k] = m_storage[k];
-    for (size_t k = num + 1; k < m_size; ++k)
-      newStorage[k - 1] = m_storage[k];
-      
-    delete[] m_storage;
-    m_storage = newStorage;
-    --m_size;
-  }
+        El* newStorage = new El[m_size - 1];
+        // Копируем имеющееся содержимое 
+        for (size_t k = 0; k < num; ++k)
+            newStorage[k] = m_storage[k];
+        for (size_t k = num + 1; k < m_size; ++k)
+            newStorage[k - 1] = m_storage[k];
 
-  size_t size() const {
-    return m_size;
-  }
+        delete[] m_storage;
+        m_storage = newStorage;
+        --m_size;
+    }
 
-  El* storage() const {
-    return m_storage;
-  }
+    size_t size() const {
+        return m_size;
+    }
 
-=======
-	Storage() m_storage(nullptr), m_size(0) {
+    El* storage() const {
+        return m_storage;
+    }
 
-	}
-	void add(const El& d) {
-		El* newStorage = new El[m_size + 1];
-		// Копируем имеющееся содержимое 
-		for (size_t k = 0; k < m_size; ++k)
-			newStorage[k] = m_storage[k];
-		newStorage[m_size] = d;
-		delete[] m_storage;
-		m_storage = newStorage;
-		++m_size;
-	}
+    El getElementPosition(int pos) {
+        if (pos < m_size && pos >= 0) {
+            return m_storage[pos];
+        }
+        throw "invalid position";
+    }
 
-El getElementPosition (int pos) {
-	if (pos < m_size && pos >= 0) {
-		return m_storage[pos];
-	}
-	throw "invalid position";
-}
-
->>>>>>> 0dbcbfd1697110fc360fbe7ab1998642d87635e5
 private:
-  El* m_storage;
-  size_t m_size;
+    El* m_storage;
+    size_t m_size;
 };
 
 Storage<dot> createTestDotStorage();
