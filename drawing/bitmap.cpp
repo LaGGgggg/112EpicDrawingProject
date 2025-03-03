9ea661ab231ff0084407e27048bc16b13f465964
LittelIrbis <postelnyhpolina@gmail.com>
2025-03-03 23:10:50 +0300

    Create bitmap.cpp

---
+++ drawing/bitmap.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include "bitmap.h"
+
+bool loadBitMap(const char* filename, BitMap&bm) {
+	std::fstream fs;
+	fs.open(filename, std::ios_base::in | std::ios_base::binary);
+	if (!fs.is_open())
+		return false;
+	const int DATA_OFFSET_POS = 10;
+	fs.seekg(DATA_OFFSET_POS);
+	int dataOffset = 0;
+	fs.read(reinterpret_cast<char*>(&dataOffset), 4);
+
+	std::cout << "Data offset " << dataOffset << std::endl;
+
+	const int WIDTH_POS = 18;
+
+	fs.read(reinterpret_cast<char*>(&bm.cols), 4);
+	std::cout << "Width " << bm.cols << std::endl;
+
+	fs.read(reinterpret_cast<char*>(&bm.rows), 4);
+	std::cout << "Height " << bm.rows << std::endl;
+
+
+	const int bytes_in_row = (bm.cols / 8) + (bm.cols % 8 ? 1 : 0) * 4;
+	std::cout << "bytes_in_row = " << bytes_in_row << std::endl;
+
+	bm.data = new uint8_t[bytes_in_row*bm.rows];
+	fs.seekg(dataOffset);
+	fs.read(reinterpret_cast<char*>(bm.data), bytes_in_row*bm.rows);
+}
+
+bool isFree(int x, int y, const BitMap&bm) {
+	if (x < 0 || x >= bm.cols || y < 0 || y >= bm.rows)
+		return false;
+
+}
