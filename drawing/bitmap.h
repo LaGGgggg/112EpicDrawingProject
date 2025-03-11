2b9306756136546d98182833d60d1a56f0ab2f6f
Arseny-P <catdog09073@gmail.com>
2025-03-11 11:16:48 +0300

    add: data in BitMap-private

--- drawing/bitmap.h
+++ drawing/bitmap.h
@@ -3,12 +3,21 @@
 
 class BitMap{
 public:
-	BitMap(uint32_t rows,uint32_t cols);
+	BitMap(uint32_t rows,uint32_t cols): rows(rows), cols(cols) {
+		data = new int[rows * cols];
+		std::fill(data, data + (rows * cols), 0);
+	}
 	void saveTo(const char* filename);
 	void loadFrom(const char* filename);
 	void setPixel(uint32_t x,uint32_t y,bool isBlack);
 	bool isBlack(uint32_t x,uint32_t y);
 	void drawSegment(uint32_t x1,uint32_t y1,  uint32_t x2, uint32_t y2,bool isBlack);
-private:
 
+	~BitMap(){
+		delete[] data;
+	}
+private:
+	uint32_t rows;
+	uint32_t cols;
+	uint32_t* data;
 };
