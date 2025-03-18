fd9ba1492a4828073d9500cba6fd5b1daebcc9aa
Arseny-P <catdog09073@gmail.com>
2025-03-18 12:15:11 +0300

    fix: redesigned the tests to remove the storage func

--- storage/tests/test_storage.cpp
+++ storage/tests/test_storage.cpp
@@ -9,8 +9,9 @@ TEST(testStorage, addDot) {
     ds.add(d1);
   
     ASSERT_EQ(ds.size(), 1);
-    ASSERT_DOUBLE_EQ(ds.storage()[0].getx(), 1.4);
-    ASSERT_DOUBLE_EQ(ds.storage()[0].gety(), 2.5);
+    dot Dot = ds.getElementPosition(0);
+    ASSERT_DOUBLE_EQ(Dot.getx(), 1.4);
+    ASSERT_DOUBLE_EQ(Dot.gety(), 2.5);
 }
   
 TEST(testStorage, addSegment){
@@ -19,8 +20,11 @@ TEST(testStorage, addSegment){
     ss.add(s1);
   
     ASSERT_EQ(ss.size(), 1);
-    ASSERT_TRUE(ss.storage()[0].getStart().getx() == s1.getStart().getx() && ss.storage()[0].getStart().gety() == s1.getStart().gety());
-    ASSERT_TRUE(ss.storage()[0].getEnd().getx() == s1.getEnd().getx() && ss.storage()[0].getEnd().gety() == s1.getEnd().gety());
+    segment seg = ss.getElementPosition(0);
+    ASSERT_EQ(seg.getStart().getx(), s1.getStart().getx());
+    ASSERT_EQ(seg.getStart().gety(), s1.getStart().gety());
+    ASSERT_EQ(seg.getEnd().getx(), s1.getEnd().getx());
+    ASSERT_EQ(seg.getEnd().gety(), s1.getEnd().gety());
 }
   
 Storage<dot> createTestDotStorage() {
@@ -39,60 +43,71 @@ TEST(testStorage, removeFirst){
     Storage<dot> ds = createTestDotStorage();
     ds.remove(0);
     ASSERT_EQ(ds.size(),2);
-    ASSERT_TRUE(ds.storage()[0].getx() == 2.2 && ds.storage()[0].gety() == 2.2);
-    ASSERT_TRUE(ds.storage()[1].getx() == 3.3 && ds.storage()[1].gety() == 3.3);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).getx(), 2.2);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).gety(), 2.2);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(1).getx(), 3.3);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(1).gety(), 3.3);
 }
 
 // test 2: del second el
 TEST(testStorage, removeSecond){
+    Storage<dot> ds = createTestDotStorage();
     ds = createTestDotStorage();
     ds.remove(1);
     ASSERT_EQ(ds.size(), 2);
-    ASSERT_TRUE(ds.storage()[0].getx() == 1.1 && ds.storage()[0].gety() == 1.1);
-    ASSERT_TRUE(ds.storage()[1].getx() == 3.3 && ds.storage()[1].gety() == 3.3);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).getx(), 1.1);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).gety(), 1.1);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(1).getx(), 3.3);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(1).gety(), 3.3);
 }
 
 // test 3: del third el
 TEST(testStorage, removeThird){
+    Storage<dot> ds = createTestDotStorage();
     ds = createTestDotStorage();
     ds.remove(2);
     ASSERT_EQ(ds.size(), 2);
-    ASSERT_TRUE(ds.storage()[0].getx() == 1.1 && ds.storage()[0].gety() == 1.1);
-    ASSERT_TRUE(ds.storage()[1].getx() == 2.2 && ds.storage()[1].gety() == 2.2);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).getx(), 1.1);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).gety(), 1.1);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(1).getx(), 2.2);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(1).gety(), 2.2);
 }
 
 // test 4: del second & third el
 TEST(testStorage, remove2elem){
+    Storage<dot> ds = createTestDotStorage();
     ds = createTestDotStorage();
     ds.remove(1);
     ds.remove(1);
     ASSERT_EQ(ds.size(), 1);
-    ASSERT_TRUE(ds.storage()[0].getx() == 1.1 && ds.storage()[0].gety() == 1.1);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).getx(), 1.1);
+    ASSERT_DOUBLE_EQ(ds.getElementPosition(0).gety(), 1.1);
 }
 
  
     //Test 1
  TEST(testStorage, getElement){
-        Storage<dot> ds = createTestDotStorage();
-        dot d1 = ds.getElementPosition(0);
-        EXPECT_DOUBLE_EQ(d1.getx(), 1.1);
-        EXPECT_DOUBLE_EQ(d1.gety(), 1.1);
-        dot d3 = ds.getElementPosition(2);
-        EXPECT_DOUBLE_EQ(d3.getx(), 3.3);
-        EXPECT_DOUBLE_EQ(d3.gety(), 3.3);
+    Storage<dot> ds = createTestDotStorage();
+    dot d1 = ds.getElementPosition(0);
+    ASSERT_DOUBLE_EQ(d1.getx(), 1.1);
+    ASSERT_DOUBLE_EQ(d1.gety(), 1.1);
+    dot d3 = ds.getElementPosition(2);
+    ASSERT_DOUBLE_EQ(d3.getx(), 3.3);
+    ASSERT_DOUBLE_EQ(d3.gety(), 3.3);
 }
 
 //Test 2 and Test 3
 TEST(testStorage, getInvalidPos){
-        Storage<dot> ds = createTestDotStorage();
-        EXPECT_THROW(ds.getElementPosition(-1), const char* error);
-        EXPECT_THROW(ds.getElementPosition(10), const char* error);
-    }
+    Storage<dot> ds = createTestDotStorage();
+    EXPECT_THROW(ds.getElementPosition(-1), const char*);
+    EXPECT_THROW(ds.getElementPosition(10), const char*);
+}
 
     //Test 4
 TEST(testStorage, getAfterRemove){
     Storage<dot> ds = createTestDotStorage();
     ds.remove(1);
     dot d = ds.getElementPosition(1);
-    ASSERT_TRUE(d.getx() == 3.3 && d.gety() == 3.3);
+    ASSERT_DOUBLE_EQ(d.getx(), 3.3)
+    ASSERT_DOUBLE_EQ(d.gety(), 3.3);
 }
