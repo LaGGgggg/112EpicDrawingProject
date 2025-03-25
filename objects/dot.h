ff3215695b26283617e5ea467c2e75afd000356a
Arseny-P <catdog09073@gmail.com>
2025-03-25 11:33:16 +0300

    new: redesigned dot class

--- objects/dot.h
+++ objects/dot.h
@@ -5,13 +5,13 @@
 class  dot {
 public:
 	dot(double x = 0.0, double y = 0.0):m_x(x),m_y(y) {
-		m_id = generateID();
+		//m_id = generateID();
 	}
 	double getx()const { return m_x; }
 	double gety()const { return m_y; }
-	ID getID() const { return m_id; }
+	//ID getID() const { return m_id; }
 private:
-	ID m_id;
+	//ID m_id;
 	double m_x, m_y;
 };
 #endif // DOT_H
