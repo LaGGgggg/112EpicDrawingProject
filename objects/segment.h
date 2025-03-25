11fcafb2ea66422e7f8aa38a91d1f69f8004fd10
Arseny-P <catdog09073@gmail.com>
2025-03-25 11:33:57 +0300

    new: redesigned segment class

--- objects/segment.h
+++ objects/segment.h
@@ -2,20 +2,19 @@
 #define SEGMENT_H
 
 #include "dot.h"
-#include "../ID/idgenerator.h"
 
 class segment {
 public:
-	segment(const dot&s = dot(), const dot& e = dot()):m_start(s),m_end(e) {
-		m_id = generateID();
+	segment(dot*s = nullptr, dot* e = nullptr):m_start(s),m_end(e) {
+		//m_id = generateID();
 	}
-	dot getStart() const {return m_start;}
-	dot getEnd() const {return m_end;}
-	ID getID() const { return m_id; }
+	dot getStart() const {return *m_start;}
+	dot getEnd() const {return *m_end;}
+	//ID getID() const { return m_id; }
 private:
-	ID m_id;
-	dot m_start;
-	dot m_end;
+	//ID m_id;
+	dot* m_start;
+	dot* m_end;
 };
 
 #endif // SEGMENT_H
