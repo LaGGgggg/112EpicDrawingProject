74d2a19117fa5c3afb376c8c63d8c3b708843fce
Arseny-P <catdog09073@gmail.com>
2025-03-25 11:58:14 +0300

    changing the dot class to a structure
    Таким образом мы в будущем сможем легче изменять положение точек и не придется реализовывать дополнительные set функции

--- objects/dot.h
+++ objects/dot.h
@@ -2,16 +2,8 @@
 #define DOT_H
 #include "../ID/idgenerator.h"
 
-class  dot {
-public:
-	dot(double x = 0.0, double y = 0.0):m_x(x),m_y(y) {
-		//m_id = generateID();
-	}
-	double getx()const { return m_x; }
-	double gety()const { return m_y; }
-	//ID getID() const { return m_id; }
-private:
-	//ID m_id;
-	double m_x, m_y;
+struct  dot {
+	double x;
+	double y;
 };
 #endif // DOT_H
