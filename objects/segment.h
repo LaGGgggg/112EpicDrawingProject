318b7cf0f1992d8532f125415cf3c9b424c938ee
Arseny-P <catdog09073@gmail.com>
2025-03-25 11:49:25 +0300

    add check for nullptr start and end

--- objects/segment.h
+++ objects/segment.h
@@ -2,14 +2,22 @@
 #define SEGMENT_H
 
 #include "dot.h"
+#include <cassert>
+#include "../ID/idgenerator.h"
 
 class segment {
 public:
 	segment(dot*s = nullptr, dot* e = nullptr):m_start(s),m_end(e) {
 		//m_id = generateID();
 	}
-	dot getStart() const {return *m_start;}
-	dot getEnd() const {return *m_end;}
+	dot getStart() const {
+		assert(m_start);
+		return *m_start;
+	}
+	dot getEnd() const {
+		assert(m_start);
+		return *m_end;
+	}
 	//ID getID() const { return m_id; }
 private:
 	//ID m_id;
