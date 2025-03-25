437e4aaa26a7286a98dde122664a9d7928a18441
Arseny-P <catdog09073@gmail.com>
2025-03-25 11:32:06 +0300

    removed Base class from main file

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "dot.h"
 #include "segment.h"
 #include "idgenerator.h"
+#include "base/base.h"
 
 enum ObjType {
 	OBJ_DOT,
@@ -12,7 +13,7 @@ enum ObjType {
 };
 
 enum RelativePosType {
-	
+	COINSIDENT_DOTS,
 	EQUAL_LENGTH_SEGMENTS, // Отрезки должны быть одинаковы по длине
 	ORTHO_SEGMENTS, // Отрезки должны быть ортогональны
 
@@ -23,67 +24,6 @@ enum SRPResult {
 	INVALID_OBJECTS,
 	INVALID_RELPOS
 };
-
-
-class Base{
-public:
-	ID addObject(ObjType otype) {
-		switch (otype) {
-		case OBJ_DOT:
-			m_dotStorage.add(dot());
-			return m_dotStorage.getElementPosition(m_dotStorage.size() - 1).getID();
-			break;
-		case OBJ_SEG:
-			m_segmentStorage.add(segment(dot(), dot()));
-			return m_segmentStorage.getElementPosition(m_segmentStorage.size() - 1).getID();
-			break;
-		}
-
-		default:
-			std::cerr << "Invalid object type" << std::endl;
-	};
-
-	SRPResult setRelativePos(Storage<ID>& objects, RelativePosType rpt) {
-		// Проверка на корректность пожеланий пользователя
-		
-		const double errorThreshold = 1e-6;
-
-		while ( getError( objects, rpt) > errorThreshold )  /* Условие необходимости продолжения процедуры поиска значений параметров*/ {
-
-			// Выбираем как изменить параметры объектов 
-			// (для этого надо уметь вычислять частные производные 
-			// от величины ошибки)
-
-			// Меняем параметры 
-			// (для этого надо реализовать методы, 
-			// модифицирующие положение точек и отрезков)
-			// Оцениваем как изменилась ситуации с точки зрения 
-			// выполнения пожеланий пользователя
-		}		
-	}
-
-	double getError(Storage<ID>& objects, RelativePosType rpt);
-
-	
-	int getObjParams(ID id, Storage<double>& params) {
-		for (size_t k = 0; k < m_dotStorage.size(); ++k) {
-			if (m_dotStorage.getElementPosition(k).getID() == id) {
-				params.add(m_dotStorage.getElementPosition(k).getx());
-				params.add(m_dotStorage.getElementPosition(k).gety());
-				return 0;
-			}
-		}
-		return -1;
-	}
-
-	
-private:
-	Storage<dot> m_dotStorage;
-	Storage<segment> m_segmentStorage;
-};
-
-
-
 
 int main() {
 	Base base;
