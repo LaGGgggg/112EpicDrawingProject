ffd8803adf1602e24eafa67ee1489fe1304256f1
Arseny-P <catdog09073@gmail.com>
2025-03-25 12:39:10 +0300

    Update base.h

--- base/base.h
+++ base/base.h
@@ -1,14 +1,16 @@
+#include "../ID/idgenerator.h"
+
 class Base{
     public:
         ID addObject(ObjType otype) {
             switch (otype) {
             case OBJ_DOT:
-                m_dotStorage.add(dot());
-                return m_dotStorage.getElementPosition(m_dotStorage.size() - 1).getID();
+                m_dotStorage.add({generateID(), new dot()});
+                return m_dotStorage[m_dotStorage.size() - 1].id;
                 break;
             case OBJ_SEG:
-                m_segmentStorage.add(segment(dot(), dot()));
-                return m_segmentStorage.getElementPosition(m_segmentStorage.size() - 1).getID();
+                m_segmentStorage.add({generateID(), new segment(dot(), dot())});
+                return m_segmentStorage[m_segmentStorage.size() - 1].id;
                 break;
             }
     
@@ -43,10 +45,19 @@ class Base{
     
         int getObjParams(ID id, Storage<double>& params) {
             for (size_t k = 0; k < m_dotStorage.size(); ++k) {
-                if (m_dotStorage.getElementPosition(k).getID() == id) {
-                    params.add(m_dotStorage.getElementPosition(k).getx());
-                    params.add(m_dotStorage.getElementPosition(k).gety());
+                if (m_dotStorage[k].id == id) {
+                    params.add(m_dotStorage[k].data->x);
+                    params.add(m_dotStorage[k].data->y);
                     return 0;
+                }
+            }
+
+            for(size_t k = 0; k < m_segmentStorage.size(); ++k){
+                if(m_segmentStorage[k].id == id){
+                    params.add( m_segmentStorage[k].data->getStart().x );
+                    params.add( m_segmentStorage[k].data->getStart().y );
+                    params.add( m_segmentStorage[k].data->getEnd().x );
+                    params.add( m_segmentStorage[k].data->getEnd().y );
                 }
             }
             return -1;
