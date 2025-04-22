dad94c5a0a6f019e161bc419506ed2ab3091f27c
Nikita <Poni22poni23@yandex.ru>
2025-04-22 11:56:22 +0300

    feat: basic use case example in `main.cpp`

--- main.cpp
+++ main.cpp
@@ -1,13 +1,74 @@
 #include "bitmap.h"
 #include "base/base.h"
+#include "ID/idgenerator.h"
+#include "objects/dot.h"
+#include "objects/segment.h"
 
 int main() {
 
-	BitMap bmp(32, 32);
-	const bool bIsBlack = true;
-	for (unsigned y = 2; y < 30; ++y)
-		bmp.setPixel(2, y,bIsBlack);
-	bmp.saveTo("vertSeg.bmp");
-	
+	Base base;
+
+	ID seg1 = base.addObject(OBJ_SEG);
+	ID seg2 = base.addObject(OBJ_SEG);
+	ID dot1 = base.addObject(OBJ_DOT);
+	ID dot2 = base.addObject(OBJ_DOT);
+
+	base.getSegmentStorage()->findElementByID(seg1)->data->updateStart(new dot(0, 2));
+	base.getSegmentStorage()->findElementByID(seg1)->data->updateEnd(new dot(0, 1));
+
+	base.getSegmentStorage()->findElementByID(seg2)->data->updateStart(new dot(1, 0));
+	base.getSegmentStorage()->findElementByID(seg2)->data->updateEnd(new dot(2.2, 1.4));
+
+	base.getDotStorage()->findElementByID(dot2)->data->x = 1;
+
+	// Create a storage for the segment IDs
+	Storage<ID> segmentStorage;
+	segmentStorage.add(seg1);
+	segmentStorage.add(seg2);
+
+	// Set their relative position to orthogonal
+	SRPResult result = base.setRelativePos(segmentStorage, ORTHO_SEGMENTS);
+	SRPResult result3 = base.setRelativePos(segmentStorage, EQUAL_LENGTH_SEGMENTS);
+
+	// Check the result
+	if (result == ALL_OK) {
+		std::cout << "Segments successfully set to orthogonal." << std::endl;
+	} else {
+		std::cerr << "Failed to set segments to orthogonal. Error code: " << result << std::endl;
+	}
+
+	Storage<ID> dotStorage;
+	dotStorage.add(dot1);
+	dotStorage.add(dot2);
+	SRPResult result2 = base.setRelativePos(dotStorage, COINSIDENT_DOTS);
+
+	if (result2 == ALL_OK) {
+		std::cout << "Dots successfully set to coincident." << std::endl;
+	} else {
+		std::cerr << "Failed to set dots to coincident. Error code: " << result2 << std::endl;
+	}
+
+	std::cout << "Segment coordinates: "
+	<< base.getSegmentStorage()->findElementByID(seg1)->data->getStart().x << ", "
+	<< base.getSegmentStorage()->findElementByID(seg1)->data->getStart().y
+	<< " "
+	<< base.getSegmentStorage()->findElementByID(seg1)->data->getEnd().x << ", "
+	<< base.getSegmentStorage()->findElementByID(seg1)->data->getEnd().y
+	<< " | "
+	<< base.getSegmentStorage()->findElementByID(seg2)->data->getStart().x << ", "
+	<< base.getSegmentStorage()->findElementByID(seg2)->data->getStart().y
+	<< " "
+	<< base.getSegmentStorage()->findElementByID(seg2)->data->getEnd().x << ", "
+	<< base.getSegmentStorage()->findElementByID(seg2)->data->getEnd().y
+	<< std::endl;
+
+	std::cout << "Dot coordinates: "
+	<< base.getDotStorage()->findElementByID(dot1)->data->x << ", "
+	<< base.getDotStorage()->findElementByID(dot1)->data->y
+	<< " "
+	<< base.getDotStorage()->findElementByID(dot2)->data->x << ", "
+	<< base.getDotStorage()->findElementByID(dot2)->data->
+	<< std::endl;
+
 	return 0;
 }
